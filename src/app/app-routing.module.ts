b1f910bbbab29fd6905b245818c4f933e1b7a6bb
paritakumbhani <46765796+paritakumbhani@users.noreply.github.com>
2021-09-06 13:36:00 -0400

    Created component with routes details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AlbumDetailsComponent } from './album-details/album-details.component';
+import { CreateAlbumComponent } from './create-album/create-album.component';
+import { LoginComponent } from './login/login.component';
+import { MyAlbumsComponent } from './my-albums/my-albums.component';
+import { PhotoDetailsComponent } from './photo-details/photo-details.component';
 import { ProfileComponent } from './profile/profile.component';
+import { UploadPictureComponent } from './upload-picture/upload-picture.component';
 
 const routes: Routes = [
-  {path:'profile', component: ProfileComponent}
+  {path:'login', component:LoginComponent},
+  {path:'albums', component:MyAlbumsComponent},
+  {path:'profile', component: ProfileComponent},
+  {path:'create', component:CreateAlbumComponent},
+  {path:'album', component:AlbumDetailsComponent},
+  {path:'upload', component:UploadPictureComponent},
+  {path:'photo', component:PhotoDetailsComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
+import { LoginComponent } from './login/login.component';
+import { MyAlbumsComponent } from './my-albums/my-albums.component';
+import { CreateAlbumComponent } from './create-album/create-album.component';
+import { AlbumDetailsComponent } from './album-details/album-details.component';
+import { UploadPictureComponent } from './upload-picture/upload-picture.component';
+import { PhotoDetailsComponent } from './photo-details/photo-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileComponent
+    ProfileComponent,
+    LoginComponent,
+    MyAlbumsComponent,
+    CreateAlbumComponent,
+    AlbumDetailsComponent,
+    UploadPictureComponent,
+    PhotoDetailsComponent
   ],
   imports: [
     BrowserModule,
